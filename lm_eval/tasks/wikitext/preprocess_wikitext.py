7f9d669b6b95b31405d8f8eac93711827b4cfaf3
KristianMin <kristian.minchev@insait.ai>
2025-10-08 13:22:59 +0000

    Remove work on token perplexity

--- lm_eval/tasks/wikitext/preprocess_wikitext.py
+++ lm_eval/tasks/wikitext/preprocess_wikitext.py
@@ -41,7 +41,6 @@ def process_results(doc, results):
     # IMPORTANT: wikitext counts number of words in *original doc before detokenization*
     _words = len(re.split(r"\s+", doc["page"]))
     _bytes = len(doc["page"].encode("utf-8"))
-    print(f"perplexity: {loglikelihood}")
     return {
         "word_perplexity": (loglikelihood, _words),
         "byte_perplexity": (loglikelihood, _bytes),
